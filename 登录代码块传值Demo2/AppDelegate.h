f1fea258ade93821407f841f3e85724e57c351b0
liuzishuo <1483843726@qq.com>
2015-10-28 22:23:42 +0800

    登录传值

---
+++ 登录代码块传值Demo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  登录代码块传值Demo2
+//
+//  Created by qingyun on 15/9/14.
+//  Copyright (c) 2015年 qingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 登录代码块传值Demo2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  登录代码块传值Demo2
+//
+//  Created by qingyun on 15/9/14.
+//  Copyright (c) 2015年 qingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
